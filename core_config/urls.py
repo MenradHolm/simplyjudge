2c8f52a4c44421fc08979c660d04e737f64d9e70
Menrad <menradholm2@gmail.com>
2026-05-24 15:48:35 +0200

    Add Simply Judge custom admin branding

--- core_config/urls.py
+++ core_config/urls.py
@@ -3,6 +3,19 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# --- CUSTOM BRANDING ---
+admin.site.site_header = "Simply Judge Administration"
+admin.site.site_title = "Simply Judge Portal"
+admin.site.index_title = "Welcome to Simply Judge"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('judging_app.urls')), 
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # This points the root website domain to your judging app
