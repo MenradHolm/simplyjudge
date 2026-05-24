b107fc1754c396ad981138efb3bfa550cec863e6
Menrad <menradholm2@gmail.com>
2026-05-24 16:25:41 +0200

    Add user registration and approval workflow

--- judging_app/urls.py
+++ judging_app/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    # Auth Routes
+    path('register/', views.register_user, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='judging_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+
+    # App Routes
     path('', views.judge_router, name='judge_router'),
     path('photo/<int:photo_id>/', views.judge_photo, name='judge_photo'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
-    # Add this public route:
     path('submit/', views.submit_photo, name='submit_photo'),
 ]
