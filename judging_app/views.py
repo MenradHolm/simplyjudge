5ce29dc0034590b2e72590d928ab1bf9615caa8e
Menrad <menradholm2@gmail.com>
2026-05-24 15:40:07 +0200

    Add login redirect to prevent 500 error

--- judging_app/views.py
+++ judging_app/views.py
@@ -1,27 +1,26 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required  # <-- Add this!
 from .models import Photo, RubricCriterion, Score
 
+@login_required(login_url='/admin/login/') # <-- Add this!
 def judge_router(request):
     """Finds the first photo this judge HAS NOT scored yet and sends them to it."""
-    # Find a photo where a score from the current user does NOT exist
     next_photo = Photo.objects.exclude(score__judge=request.user).first()
     
     if next_photo:
         return redirect('judge_photo', photo_id=next_photo.id)
     
-    # If no unscored photos are left, they are done!
     return render(request, 'judging_app/done.html')
 
+@login_required(login_url='/admin/login/') # <-- Add this!
 def judge_photo(request, photo_id):
     """Displays a single photo and handles the score saving."""
     photo = get_object_or_404(Photo, id=photo_id)
     rubric = RubricCriterion.objects.all()
     
-    # Optional: Calculate progress to show the judge
     total_photos = Photo.objects.count()
     scored_photos = Score.objects.filter(judge=request.user).count()
     
-    # If the judge clicked "Save Score"
     if request.method == "POST":
         criteria_scores = {}
         total_score = 0.0
@@ -38,7 +37,6 @@ def judge_photo(request, photo_id):
             
         comment = request.POST.get('comment', '')
         
-        # Save the score
         Score.objects.update_or_create(
             photo=photo,
             judge=request.user,
@@ -48,10 +46,8 @@ def judge_photo(request, photo_id):
                 'comment': comment
             }
         )
-        # Redirect back to the router to automatically fetch the NEXT photo!
         return redirect('judge_router')
 
-    # If it's just a normal page load, show the photo and form
     context = {
         'photo': photo,
         'rubric': rubric,
